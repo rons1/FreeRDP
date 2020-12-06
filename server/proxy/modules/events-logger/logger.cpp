fd4955b7c3624a3d342c2a9cfd67ca9901317ecb
Your Name <you@example.com>
2020-12-06 13:32:46 +0000

    server: proxy: events logger: send mouse events to postgres

--- server/proxy/modules/events-logger/logger.cpp
+++ server/proxy/modules/events-logger/logger.cpp
@@ -19,30 +19,33 @@
 
 #include <iostream>
 #include <sstream>
+#include <cinttypes>
+#include <iomanip>
 #include <ctime>
 #include <chrono>
-
+#include <libpq-fe.h>
 #include <winpr/winsock.h>
+
 #include "modules_api.h"
 #include "pf_config.h"
 
 #define TAG MODULE_TAG("input_events_logger")
 
-static constexpr char plugin_name[] = "demo";
-static constexpr char plugin_desc[] = "this is a test plugin";
+static constexpr char plugin_name[] = "events logger";
+static constexpr char plugin_desc[] = "logs mouse events to postgres";
 
 static proxyPluginsManager* g_plugins_manager = NULL;
 static SOCKET sock = -1;
 static struct sockaddr_in servaddr;
 
+static PGconn* g_conn = NULL;
+static wMessageQueue* g_queue;
+static HANDLE g_processor_thread = NULL;
+static int g_count = 0;
+std::string g_insert_query = "";
+
 #define KBD_EVENT 0
 #define MOUSE_EVENT 1
-
-typedef struct plugin_data
-{
-	HANDLE thread;
-	wMessageQueue* queue;
-} pluginData;
 
 typedef struct config
 {
@@ -51,6 +54,8 @@ typedef struct config
 } pluginConfig;
 
 static pluginConfig config = { 0 };
+static BOOL logger_plugin_insert_event_to_postgres(proxyData* pdata, UINT64 now, UINT16 flags,
+                                                   UINT16 x, UINT16 y);
 
 static BOOL logger_plugin_config_load()
 {
@@ -103,7 +108,6 @@ static void logger_plugin_log_kbd_event(proxyData* pdata, UINT64 now, UINT16 fla
 	std::string log = out.str();
 
 	logger_plugin_send_log_over_udp(log);
-	// std::cout << log << std::endl;
 }
 
 static void logger_plugin_log_mouse_event(proxyData* pdata, UINT64 now, UINT16 flags, UINT16 x,
@@ -116,29 +120,28 @@ static void logger_plugin_log_mouse_event(proxyData* pdata, UINT64 now, UINT16 f
 	std::string log = out.str();
 
 	logger_plugin_send_log_over_udp(log);
-	// std::cout << log << std::endl;
+	logger_plugin_insert_event_to_postgres(pdata, now, flags, x, y);
 }
 
 static DWORD WINAPI logger_plugin_events_handler_thread(LPVOID arg)
 {
-	proxyData* pdata = static_cast<proxyData*>(arg);
-	pluginData* p = static_cast<pluginData*>(g_plugins_manager->GetPluginData(plugin_name, pdata));
 	DWORD rc = 0;
 	wMessage message;
 	UINT64 message_time;
+	proxyData* pdata;
 
 	WLog_INFO(TAG, "events handler thread started");
 
 	while (TRUE)
 	{
-		if (!MessageQueue_Wait(p->queue))
+		if (!MessageQueue_Wait(g_queue))
 		{
 			WLog_ERR(TAG, "MessageQueue_Wait failed!");
 			rc = ERROR_INTERNAL_ERROR;
 			break;
 		}
 
-		if (!MessageQueue_Peek(p->queue, &message, TRUE))
+		if (!MessageQueue_Peek(g_queue, &message, TRUE))
 		{
 			WLog_ERR(TAG, "MessageQueue_Peek failed!");
 			rc = ERROR_INTERNAL_ERROR;
@@ -148,7 +151,8 @@ static DWORD WINAPI logger_plugin_events_handler_thread(LPVOID arg)
 		if (message.id == WMQ_QUIT)
 			break;
 
-		message_time = (UINT64)message.context;
+		UINT64 now = get_current_epoch_time();
+		pdata = (proxyData*)message.context;
 
 		switch (message.id)
 		{
@@ -162,7 +166,7 @@ static DWORD WINAPI logger_plugin_events_handler_thread(LPVOID arg)
 			{
 				UINT16 flags = (UINT16)(UINT64)message.wParam;
 				UINT16 rdp_scan_code = (UINT16)(UINT64)message.lParam;
-				logger_plugin_log_kbd_event(pdata, message_time, flags, rdp_scan_code);
+				logger_plugin_log_kbd_event(pdata, now, flags, rdp_scan_code);
 				break;
 			}
 
@@ -172,8 +176,9 @@ static DWORD WINAPI logger_plugin_events_handler_thread(LPVOID arg)
 				UINT32 pos = (UINT32)(UINT64)message.lParam;
 				UINT16 x = ((pos & 0xFFFF0000) >> 16);
 				UINT16 y = (pos & 0x0000FFFF);
+				pdata = (proxyData*)message.context;
 
-				logger_plugin_log_mouse_event(pdata, message_time, flags, x, y);
+				logger_plugin_log_mouse_event(pdata, now, flags, x, y);
 				break;
 			}
 		}
@@ -183,34 +188,10 @@ static DWORD WINAPI logger_plugin_events_handler_thread(LPVOID arg)
 	return rc;
 }
 
-static void plugin_data_free(pluginData* pd)
-{
-	MessageQueue_Free(pd->queue);
-	CloseHandle(pd->thread);
-	free(pd);
-}
-
-static pluginData* plugin_data_new()
-{
-	pluginData* pd = (pluginData*)calloc(1, sizeof(pluginData));
-
-	pd->queue = MessageQueue_New(NULL);
-	if (!pd->queue)
-		goto error;
-
-	return pd;
-error:
-	plugin_data_free(pd);
-	return NULL;
-}
-
 static BOOL logger_plugin_keyboard_event(proxyData* pdata, void* param)
 {
 	auto event = static_cast<proxyKeyboardEventInfo*>(param);
-	auto pd = static_cast<pluginData*>(g_plugins_manager->GetPluginData(plugin_name, pdata));
-	UINT64 now = get_current_epoch_time();
-
-	MessageQueue_Post(pd->queue, (void*)now, KBD_EVENT, (void*)(size_t)event->flags,
+	MessageQueue_Post(g_queue, (void*)pdata, KBD_EVENT, (void*)(size_t)event->flags,
 	                  (void*)(size_t)event->rdp_scan_code);
 	return TRUE;
 }
@@ -218,46 +199,10 @@ static BOOL logger_plugin_keyboard_event(proxyData* pdata, void* param)
 static BOOL logger_plugin_mouse_event(proxyData* pdata, void* param)
 {
 	auto event = static_cast<proxyMouseEventInfo*>(param);
-	auto pd = static_cast<pluginData*>(g_plugins_manager->GetPluginData(plugin_name, pdata));
-	UINT64 now = get_current_epoch_time();
 	UINT32 pos = (event->x << 16) | event->y;
 
-	MessageQueue_Post(pd->queue, (void*)now, MOUSE_EVENT, (void*)(size_t)event->flags,
+	MessageQueue_Post(g_queue, (void*)pdata, MOUSE_EVENT, (void*)(size_t)event->flags,
 	                  (void*)(size_t)pos);
-	return TRUE;
-}
-
-static BOOL logger_plugin_server_post_connect(proxyData* pdata)
-{
-	pluginData* p = plugin_data_new();
-	if (!p)
-		return FALSE;
-
-	g_plugins_manager->SetPluginData(plugin_name, pdata, p);
-
-	if (!(p->thread = CreateThread(NULL, 0, logger_plugin_events_handler_thread, pdata, 0, NULL)))
-		return FALSE;
-
-	return TRUE;
-}
-
-static BOOL logger_plugin_session_end(proxyData* pdata)
-{
-	auto pd = static_cast<pluginData*>(g_plugins_manager->GetPluginData(plugin_name, pdata));
-	if (!pd)
-		return TRUE;
-
-	MessageQueue_PostQuit(pd->queue, 0);
-
-	WLog_INFO(TAG, "closing events thread");
-	if (WaitForSingleObject(pd->thread, INFINITE) == WAIT_FAILED)
-	{
-		DWORD rc = GetLastError();
-		WLog_ERR(TAG, "WaitForSingleObject failed with error %" PRIu32 "", rc);
-	}
-	WLog_INFO(TAG, "events thread closed");
-
-	plugin_data_free(pd);
 	return TRUE;
 }
 
@@ -284,27 +229,88 @@ static BOOL logger_plugin_unload()
 	/* free config */
 	free(config.ip_address);
 
+	MessageQueue_PostQuit(g_queue, 0);
+
+	WLog_INFO(TAG, "closing events thread");
+	if (WaitForSingleObject(g_processor_thread, INFINITE) == WAIT_FAILED)
+	{
+		DWORD rc = GetLastError();
+		WLog_WARN(TAG, "WaitForSingleObject failed with error %" PRIu32 "", rc);
+	}
+
+	WLog_INFO(TAG, "events thread closed");
+
+	if (g_count > 0)
+	{
+		// Insert last batch of logs to database.
+		PGresult* res = PQexec(g_conn, g_insert_query.c_str());
+		WLog_INFO(TAG, "Executing batch: inserting last batch of %d rows", g_count);
+	
+		if (PQresultStatus(res) != PGRES_COMMAND_OK)
+			WLog_ERR(TAG, "couldn't insert row to databse, error code: 0x%x\n", PQresultStatus(res));
+
+		PQclear(res);
+		g_count = 0;
+		g_insert_query = "";
+	}
+
 	closesocket(sock);
+	MessageQueue_Free(g_queue);
+	PQfinish(g_conn);
+	CloseHandle(g_processor_thread);
 	return TRUE;
 }
 
 static proxyPlugin demo_plugin = {
-	plugin_name,                       /* name */
-	plugin_desc,                       /* description */
-	logger_plugin_unload,              /* PluginUnload */
-	NULL,                              /* ClientPreConnect */
-	NULL,                              /* ClientPostConnect */
-	NULL,                              /* ClientLoginFailure */
-	NULL,                              /* ClientEndPaint */
-	logger_plugin_server_post_connect, /* ServerPostConnect */
-	NULL,                              /* ServerChannelsInit */
-	NULL,                              /* ServerChannelsFree */
-	logger_plugin_session_end,         /* ServerSessionEnd */
-	logger_plugin_keyboard_event,      /* KeyboardEvent */
-	logger_plugin_mouse_event,         /* MouseEvent */
-	NULL,                              /* ClientChannelData */
-	NULL,                              /* ServerChannelData */
+	plugin_name,                  /* name */
+	plugin_desc,                  /* description */
+	logger_plugin_unload,         /* PluginUnload */
+	NULL,                         /* ClientPreConnect */
+	NULL,                         /* ClientPostConnect */
+	NULL,                         /* ClientLoginFailure */
+	NULL,                         /* ClientEndPaint */
+	NULL,                         /* ServerPostConnect */
+	NULL,                         /* ServerChannelsInit */
+	NULL,                         /* ServerChannelsFree */
+	NULL,                         /* ServerSessionEnd */
+	logger_plugin_keyboard_event, /* KeyboardEvent */
+	logger_plugin_mouse_event,    /* MouseEvent */
+	NULL,                         /* ClientChannelData */
+	NULL,                         /* ServerChannelData */
 };
+
+static BOOL logger_plugin_insert_event_to_postgres(proxyData* pdata, UINT64 now, UINT16 flags,
+                                                   UINT16 x, UINT16 y)
+{
+	std::ostringstream ss;
+	ss << "INSERT INTO playground (time, sessionid, flags, scancode, x, y, username) VALUES(" << now
+	   << ",'" << pdata->session_id << "'," << flags << "," << 0 << "," << x << "," << y << ",'"
+	   << pdata->pc->context.settings->Username << "');";
+	g_insert_query.append(ss.str());
+
+	if (g_count == 1000)
+	{
+		PGresult* res = PQexec(g_conn, g_insert_query.c_str());
+		WLog_INFO(TAG, "Executing batch: inserting 1000 rows");
+
+		if (PQresultStatus(res) != PGRES_COMMAND_OK)
+		{
+			WLog_ERR(TAG, "couldn't insert row to databse, error code: 0x%x\n",
+			         PQresultStatus(res));
+			PQclear(res);
+			return FALSE;
+		}
+
+		WLog_INFO(TAG, "reset batch count");
+		g_insert_query = "";
+		g_count = 0;
+		PQclear(res);
+	}
+
+	WLog_DBG(TAG, "row %d: adding to next batch", g_count);
+	g_count++;
+	return TRUE;
+}
 
 BOOL proxy_module_entry_point(proxyPluginsManager* plugins_manager)
 {
@@ -317,7 +323,19 @@ BOOL proxy_module_entry_point(proxyPluginsManager* plugins_manager)
 	if (sock == -1)
 		return FALSE;
 
+	g_queue = MessageQueue_New(NULL);
+	if (!(g_processor_thread =
+	          CreateThread(NULL, 0, logger_plugin_events_handler_thread, NULL, 0, NULL)))
+		return FALSE;
+
 	WLog_INFO(TAG, "Logger plugin is initialized. logs will be sent to %s:%d.", config.ip_address,
 	          config.port);
+	// Connect to PostgRES.
+	g_conn = PQconnectdb("user=kobi password=Dadush1020304050 dbname=plex");
+	if (PQstatus(g_conn) == CONNECTION_BAD)
+	{
+		fprintf(stderr, "Connection to database failed: %s", PQerrorMessage(g_conn));
+	}
 	return plugins_manager->RegisterPlugin(&demo_plugin);
 }
+
